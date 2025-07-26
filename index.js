724ea92fee40c59a6fd6d4abf0931f0921ebc2e7
PvNastia <nastiapogorelova6@gmail.com>
2025-07-26 12:05:32 +0300

    add

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const hiddenElements = document.querySelectorAll('.hidden-text');
-
+const hiddenImg = document.querySelector('.hidden-img');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
+      hiddenImg.classList.add('active');
     }
   });
 });
